0ca72bfa46ff08fbcbe2bb79fae3a7abcb50d9af
kiirzz <hmtb2001@gmail.com>
2025-01-22 23:57:25 +0700

    Fix bug

--- Lab11/obj/Debug/net9.0/Lab11.AssemblyInfo.cs
+++ Lab11/obj/Debug/net9.0/Lab11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0ab12e1cc119ea18a532a19e5b4d385beb9564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
